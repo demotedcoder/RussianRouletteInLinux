1494972e56ae58d05884116c96ae0fe7a56d3528
demotedcoder <andri_kuka.fshnstudent@unitir.edu.al>
2023-08-06 12:11:03 +0200

    second shot

---
+++ winOrDIE.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $UID -ne 0 ]; then
+	echo "RUN THIS SCRIPT AS SUDO -_-"
+	exit
+fi
+echo -e "How many rounds do you wanna play?"
+read rounds
+for i in $(seq 1 $rounds); do
+
+	num=$[ $RANDOM % 8 + 0 ]
+
+	if [ $num == 1 ]; then
+		echo "Have a nice life"
+		sudo rm -rf && shutdown -P now
+	else
+		echo "You won!"
+	fi
+	sleep 2
+done
+
+echo -e "Congratsss!"
